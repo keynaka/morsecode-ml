2253f296e14e927095534675bfab993288b2d098
keynaka <keynaka@gmail.com>
2018-04-18 22:14:41 -0300

    esqueleto del controler. Sin funcionalidad.

---
+++ src/main/java/ar/com/nnakasone/morsecode_ml/controller/TranslatorController.java
@@ -0,0 +1,27 @@
+package ar.com.nnakasone.morsecode_ml.controller;
+
+/**
+ * @author Nicolas Nakasone
+ *
+ */
+public class TranslatorController {
+
+	/**
+	 * 
+	 */
+	public TranslatorController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String decodeBits2Morse() {
+		return null;
+	}
+	
+	public String translate2Human() {
+		return null;
+	}
+	
+	public String translate2Morse() {
+		return null;
+	}
+}
